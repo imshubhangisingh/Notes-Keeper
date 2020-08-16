7c0cd6ba2a994bc30969f296bf6343cd62a8ea32
imshubhangisingh <shubhangi.singh29@gmail.com>
2020-08-16 11:43:49 +0530

    Code for server.js

---
+++ server/server.js
@@ -0,0 +1,40 @@
+//importing express library
+const express = require("express");
+
+//Importing morgan, cors and session.
+const morgan = require("morgan");
+const cors = require("cors");
+const session = require("express-session");
+
+//Creating instance of the Express library to do our app.
+const app = express();
+
+//Defining a part for Express App to listen to.
+const port = 3000;
+
+//get routes.
+const users = require("./routes/users");
+const root = require("./routes/root");
+const notes = require("./routes/notes");
+
+//Added middleware
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+app.use(
+  session({
+    secret: "my-secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+  })
+);
+
+//Use the routes.
+app.use("/", root);
+app.use("/users", users);
+app.use("/notes", notes);
+
+app.listen(port, () => {
+  console.log("Successfully started the server on port " + port);
+});
