5921486f710fad9a8c474cb49ece9fdd892b6eac
imshubhangisingh <shubhangi.singh29@gmail.com>
2020-08-17 21:18:42 +0530

    Implemented the Login form HTML alone

--- client/src/Component/App.js
+++ client/src/Component/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import Header from './Shared/Header.js';
+import Login from './Login/Login'
 
 
 class App extends Component {
   state = {
-    LoggedIn: false
+    LoggedIn: true
   };
   render() {
     return (
       <div className='App'>
         <Header dark={true}>Leadstagram1</Header>
-        {this.state.LoggedIn ? <p>Show Notes</p> : <p>Show Login</p>}
+        {this.state.LoggedIn ? <Login /> : <p>Show Notes</p>}
       </div>
     );
   }
