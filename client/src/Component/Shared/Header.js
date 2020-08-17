1b3a7fd14ac2cf274eafc506bd3cb7eed53b5060
imshubhangisingh <shubhangi.singh29@gmail.com>
2020-08-17 21:40:26 +0530

    .

--- client/src/Component/Shared/Header.js
+++ client/src/Component/Shared/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 const Header = ({ dark, children, className, pages }) => {
   dark = !!dark ? 'dark' : 'light';
