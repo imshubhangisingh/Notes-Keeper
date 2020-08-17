611b52d70d049ec5a75c01ae6447a9aa7f0952ef
imshubhangisingh <shubhangi.singh29@gmail.com>
2020-08-18 00:29:12 +0530

    Implemented logout on server side

--- client/src/Services/AuthServices.js
+++ client/src/Services/AuthServices.js
@@ -2,3 +2,7 @@ import Axios from "axios"
 
 //will return a promise
 export const AuthenticateUser = (cred) => Axios.post("/api/users/login", cred);
+
+export const LogoutUser = () => {
+  Axios.post("/api/users/logout");
+}
