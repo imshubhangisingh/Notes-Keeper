7480f00bc91905d4e7d040833202664494727923
imshubhangisingh <shubhangi.singh29@gmail.com>
2020-08-18 19:55:00 +0530

    Success Message

--- client/src/Component/App.js
+++ client/src/Component/App.js
@@ -8,42 +8,47 @@ import { AuthenticateUser, LogoutUser } from '../Services/AuthServices.js';
 class App extends Component {
   state = {
     LoggedIn: false,
-    Error: false
+    Error: false,
+    Success: false
   };
   handelAuthentication = cred => {
     AuthenticateUser(cred)
       .then(res => {
         this.setState({
           LoggedIn: res.data,
-          Error: false
+          Error: false,
+          Success: false
         });
       }).catch(() => {
         this.setState({
           LoggedIn: false,
-          Error: true
+          Error: true,
+          Success: false
         });
       });
   };
 
   handelLogout = () => {
-    LogoutUser().then(res => {
+    LogoutUser().then((res) => {
       if (res.data === "User successfully logged out.")
         this.setState({
           LoggedIn: false,
-          Error: false
+          Error: false,
+          Success: true
         });
-    })
-
-  }
+    });
+  };
   render() {
     return (
       <div className='App'>
-        <Header dark={true}>Leadstagram1</Header>
+        <Header dark={true}>Keeper</Header>
         {this.state.LoggedIn ? (
           <Notes LoggedIn={this.state.LoggedIn} Logout={this.handelLogout} />
         ) : (
-            <Login handelAuthentication={this.handelAuthentication}
+            <Login
+              handelAuthentication={this.handelAuthentication}
               Error={this.state.Error}
+              Success={this.state.Success}
             />
           )}
       </div>
