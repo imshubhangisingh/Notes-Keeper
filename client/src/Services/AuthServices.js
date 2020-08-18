4ddaadcdfe50c29aefb5b67a9b115723dc4a734d
imshubhangisingh <shubhangi.singh29@gmail.com>
2020-08-18 19:54:44 +0530

    Added success message and remove the ".then" error in AuthServices.js

--- client/src/Services/AuthServices.js
+++ client/src/Services/AuthServices.js
@@ -3,8 +3,7 @@ import Axios from "axios"
 //will return a promise
 export const AuthenticateUser = (cred) => Axios.post("/api/users/login", cred);
 
-export const LogoutUser = () => {
-  Axios.post("/api/users/logout");
-}
+export const LogoutUser = () => Axios.post("/api/users/logout");
+
 
 export const RegisterUser = data => Axios.post("/api/users/new", data);
