ae268d6c0fb33a3e65b1a573833c62a95bdc7c1a
imshubhangisingh <shubhangi.singh29@gmail.com>
2020-08-19 01:07:38 +0530

    Showed different routes based on container

--- client/src/Component/Notes/_NotesContainer.js
+++ client/src/Component/Notes/_NotesContainer.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link, useRouteMatch } from "react-router-dom"
 
 const NotesContainer = () => {
+  const urm = useRouteMatch();
   return (
     <div className="col-10">
-      <p>This is Notes Container!</p>
+      <p>
+        {urm.path === "/"
+          ? "You are in the home page"
+          : "You are in" + urm.params.noteID}
+      </p>
     </div>
   )
 }
