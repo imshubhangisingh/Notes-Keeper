f968956ef6f62751eb2f125efa21ea074a0e1a3a
imshubhangisingh <shubhangi.singh29@gmail.com>
2020-08-19 23:32:20 +0530

    Unified case for notrid property

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -11,10 +11,14 @@ if (!fs.existsSync(__dirname + "/../storage"))
 if (!fs.existsSync(__dirname + "/../storage/notes.json"))
   fs.writeFileSync(
     __dirname + "/../storage/notes.json", "[]");
-const notes = require("../storage/notes.json").map((note, NoteID) => ({
+const notes = require("../storage/notes.json").map((note, noteid) => ({
   ...note,
-  NoteID
+  noteid
 }));
+
+const GetNoteById = NoteID => note.find(note => note.noteid === +NoteID);
+
+
 
 //work with notes only if user is logged in
 app.use((req, res, next) => {
