6336f0cf191be1ea42e8e21d430d0d3defefde60
imshubhangisingh <shubhangi.singh29@gmail.com>
2020-08-19 18:22:35 +0530

    imported index.scss

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './Component/App';
 import * as serviceWorker from './tests/serviceWorker';
+import "./styles/index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
